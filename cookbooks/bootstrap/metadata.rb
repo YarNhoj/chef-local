5362bf70c4cd31d11b1a04719d053ab26aa12a00
John R. Ray <jray@shadow-soft.com>
2015-12-07 19:55:33 -0500

    chef-local initial commit

---
+++ cookbooks/bootstrap/metadata.rb
@@ -0,0 +1,7 @@
+name 'bootstrap'
+maintainer 'John R. Ray'
+maintainer_email 'jray@shadow-soft.com'
+license 'all_rights'
+description 'Installs/Configures bootstrap'
+long_description 'Installs/Configures bootstrap'
+version '0.1.0'
---
+++ cookbooks/bootstrap/recipes/default.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: bootstrap
+# Recipe:: default
+#
+# Copyright (c) 2015 John R. Ray, All Rights Reserved.
+# This will install RPMS's and call other cookbooks?
+#
+#
+
+Apps
+  Vagrant
+  Virtual Box
+  ChefDk
+  Docker
+  OpenVPN
+  CiscoAnyConnect
+  VSCode
+  google chrome
+
+RPMS
+  wget
+  git
+  curl
+  ansible
+
+System
+  rvm?
+  user creation
+  home directory setup
+  ssh
+  bash
+  vim
+  tmux
+
