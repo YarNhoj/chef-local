2cb34b41726e06f0e1af6438fb9dbf5d197ecac8
YarNhoj <jray@shadow-soft.com>
2015-12-07 21:18:22 -0500

    update for syntax

--- cookbooks/bootstrap/recipes/default.rb
+++ cookbooks/bootstrap/recipes/default.rb
@@ -33,8 +33,8 @@
 #  tmux
 
 begin
-  ga = data_bag_item("apps","global")
-  u  = data_bag_item("config", Etc.getlogin)
+  ga = data_bag_item('apps','global')
+  u  = data_bag_item('config', Etc.getlogin)
 rescue Net::HTTPServerException => e
   Chef::Application.fatal!("#{cookbook_name} could not load data bag; #{e}")
 end
@@ -49,9 +49,10 @@ u['dirs'].each do |dir|
   directory "#{ENV['HOME']}/#{dir}" do
     recursive true
   end
+end
 
-if u.has_key?("repos")
-  u["repos"].each do |target, repo|
+if u.has_key?('repos')
+  u['repos'].each do |target, repo|
     git "#{ENV['HOME']}/#{target}" do
       repository repo['repo']
       reference repo['revision']
